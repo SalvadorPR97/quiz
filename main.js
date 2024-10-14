7432838c72bfaae3d3279c9ec7cb57cf68b68093
Salvi <s.perezranchal@gmail.com>
2024-10-14 09:54:31 +0200

    se generan las respuestas dinámicamente

--- main.js
+++ main.js
@@ -4,10 +4,28 @@ const body = document.querySelector("body");
 const tittle = "Quiz Question";
 const questions = ["What is the capital of France?","What is the longest river in the world?","Who wrote Romeo and Juliet?",
   "How many planets are there in our solar system?"];
+  const ANSWERS = [["London", "Berlín", "Paris", "Madrid"],
+  ["Amazonas", "Nilo", "Yangtsé", "Miño"],
+  ["Jane Austen", "Cervantes", "William Shakerpeare", "Charles Dickens"],
+  ["7", "8", "9", "10"]]
 let questionCounter = 0;
-const answers = ["London","Berlin","Paris","Madrid"]
 
 const controllersText = ["Preview", "Next"];
+
+// Función para generar las respuestas 
+function generarRespuestas (indice){
+  while (ul1.firstChild){
+    ul1.removeChild(ul1.firstChild);
+  };
+  ANSWERS[indice].forEach(answer => {
+    let btn = document.createElement("button");
+    let li = document.createElement("li");
+    btn.className = "answer-btn";
+    btn.textContent = answer;
+    li.append(btn);
+    ul1.append(li);
+  });
+}
 
 const div1 = document.createElement("div");
 div1.className = "container";
@@ -21,14 +39,7 @@ p1.textContent = questions[questionCounter];
 const ul1 = document.createElement("ul");
 ul1.className = "container-answers"
 
-answers.forEach(answer => {
-  let btn = document.createElement("button");
-  let li = document.createElement("li");
-  btn.className = "answer-btn";
-  btn.textContent = answer;
-  li.append(btn);
-  ul1.append(li);
-});
+generarRespuestas(questionCounter);
 
 const div2 = document.createElement("div");
 div2.className = "container-footer";
@@ -42,7 +53,8 @@ controllersText.forEach(controllerText => {
 
 body.append(div1);
 div1.append(h2_1, p1, ul1, div2);
-//Funcionalidad
+
+//Funcionalidad botones
 
 let footerBtns = document.querySelectorAll(".footer-btn");
 let btnPreview = footerBtns[0];
@@ -58,9 +70,12 @@ btnNext.addEventListener("click", () =>{
   }
   questionCounter++;
   p1.textContent = questions[questionCounter];
+  
+  generarRespuestas(questionCounter);
   if (p1.textContent == questions.at(-1)){
     btnNext.disabled = true;
   }
+  
 })
 //Evento de botón preview
 btnPreview.addEventListener("click", () =>{
@@ -69,6 +84,7 @@ btnPreview.addEventListener("click", () =>{
   }
   questionCounter--;
   p1.textContent = questions[questionCounter];
+  generarRespuestas(questionCounter);
   if (p1.textContent == questions.at(0)){
     btnPreview.disabled = true;
   }
