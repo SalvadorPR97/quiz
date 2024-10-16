561f5f8d6930afb9da0754bb4ce7544cf3247c0c
Salvi <s.perezranchal@gmail.com>
2024-10-16 09:13:22 +0200

    refacorizacion a uso de literal objects

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ import './style.css'
 
 const body = document.querySelector("body");
 const tittle = "Quiz Question";
-const questions = ["What is the capital of France?","What is the longest river in the world?","Who wrote Romeo and Juliet?",
-  "How many planets are there in our solar system?"];
-  const ANSWERS = [["London", "Berlín", "Paris", "Madrid"],
-  ["Amazonas", "Nilo", "Yangtsé", "Miño"],
-  ["Jane Austen", "Cervantes", "William Shakerpeare", "Charles Dickens"],
-  ["7", "8", "9", "10"]]
+const quizElements = [{"question": "What is the capital of France?", "answers": ["London", "Berlín", "Paris", "Madrid"]},
+  {"question": "What is the longest river in the world?", "answers": ["Amazonas", "Nilo", "Yangtsé", "Miño"] },
+  {"question": "Who wrote Romeo and Juliet?", "answers": ["Jane Austen", "Cervantes", "William Shakerpeare", "Charles Dickens"] },
+  {"question" : "How many planets are there in our solar system?" , "answers" : ["7", "8", "9", "10"]}];
+
 let questionCounter = 0;
 
 const controllersText = ["Preview", "Next"];
@@ -17,7 +16,7 @@ function generarRespuestas (indice){
   while (ul1.firstChild){
     ul1.removeChild(ul1.firstChild);
   };
-  ANSWERS[indice].forEach(answer => {
+  quizElements[indice]["answers"].forEach(answer => {
     let btn = document.createElement("button");
     let li = document.createElement("li");
     btn.className = "answer-btn";
@@ -34,7 +33,7 @@ const h2_1 = document.createElement("h2");
 h2_1.textContent = tittle;
 
 const p1 = document.createElement("p");
-p1.textContent = questions[questionCounter];
+p1.textContent = quizElements[questionCounter]["question"];
 
 const ul1 = document.createElement("ul");
 ul1.className = "container-answers"
@@ -60,32 +59,32 @@ let footerBtns = document.querySelectorAll(".footer-btn");
 let btnPreview = footerBtns[0];
 let btnNext = footerBtns[1];
 // Primer inicio de quiz
-if (p1.textContent == questions[0]){
+if (p1.textContent == quizElements[0]["question"]){
   btnPreview.disabled = true;
 }
+//Evento de botón preview
+btnPreview.addEventListener("click", () =>{
+  if (questionCounter == quizElements.length -1){
+    btnNext.disabled = false;
+  }
+  questionCounter--;
+  p1.textContent = quizElements[questionCounter]["question"];
+  generarRespuestas(questionCounter);
+  if (p1.textContent == quizElements[0]["question"]){
+    btnPreview.disabled = true;
+  }
+})
 //Evento de botón Next
 btnNext.addEventListener("click", () =>{
   if (questionCounter == 0){
     btnPreview.disabled = false;
   }
   questionCounter++;
-  p1.textContent = questions[questionCounter];
+  p1.textContent = quizElements[questionCounter]["question"];
   
   generarRespuestas(questionCounter);
-  if (p1.textContent == questions.at(-1)){
+  if (p1.textContent == quizElements.at(-1)["question"]){
     btnNext.disabled = true;
   }
   
-})
-//Evento de botón preview
-btnPreview.addEventListener("click", () =>{
-  if (questionCounter == questions.length -1){
-    btnNext.disabled = false;
-  }
-  questionCounter--;
-  p1.textContent = questions[questionCounter];
-  generarRespuestas(questionCounter);
-  if (p1.textContent == questions.at(0)){
-    btnPreview.disabled = true;
-  }
 })
