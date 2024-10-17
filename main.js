4602fde9136994de98a6f7d5b8bcc5e38305eb07
Salvi <s.perezranchal@gmail.com>
2024-10-17 12:44:33 +0200

    !mostrando modal

--- main.js
+++ main.js
@@ -1,48 +1,83 @@
-import './style.css'
+import "./style.css";
 
 const body = document.querySelector("body");
 const tittle = "Quiz Question";
-const quizElements = [{"question": "What is the capital of France?", "answers": ["London", "Berlín", "Paris", "Madrid"]},
-  {"question": "What is the longest river in the world?", "answers": ["Amazonas", "Nilo", "Yangtsé", "Miño"] },
-  {"question": "Who wrote Romeo and Juliet?", "answers": ["Jane Austen", "Cervantes", "William Shakerpeare", "Charles Dickens"] },
-  {"question" : "How many planets are there in our solar system?" , "answers" : ["7", "8", "9", "10"]}];
+const quizElements = [
+  {
+    question: "What is the capital of France?",
+    answers: ["London", "Berlín", "Paris", "Madrid"],
+    correctAnswer: "Paris",
+  },
+  {
+    question: "What is the longest river in the world?",
+    answers: ["Amazonas", "Nilo", "Yangtsé", "Miño"],
+    correctAnswer: "Nilo",
+  },
+  {
+    question: "Who wrote Romeo and Juliet?",
+    answers: [
+      "Jane Austen",
+      "Cervantes",
+      "William Shakerpeare",
+      "Charles Dickens",
+    ],
+    correctAnswer: "William Shakerpeare",
+  },
+  {
+    question: "How many planets are there in our solar system?",
+    answers: ["7", "8", "9", "10"],
+    correctAnswer: "8",
+  },
+];
 
 let questionCounter = 0;
+let todosTienenRespuesta = false;
+const controllersText = ["Preview", "Next", "Check"];
 
-const controllersText = ["Preview", "Next"];
 //Función para marcar respuestas
-function marcarRespuesta(btn) { 
-  quizElements[questionCounter]["num_respuesta"] = btn.getAttribute("num_respuesta");
+function marcarRespuesta(btn) {
+  quizElements[questionCounter]["respuesta"] = btn.textContent;
   ul1.childNodes.forEach((li) => {
     li.firstChild.style.backgroundColor = "#f8f8f8";
     li.firstChild.choosed = "false";
   });
-  btn.style.backgroundColor = "#3CB371"; 
+  btn.style.backgroundColor = "#3CB371";
   btn.choosed = "true";
 }
-//
+// Marca la respuesta correcta seleccionada anteriormente
 function marcarCorrecta() {
   ul1.childNodes.forEach((li) => {
-    if (li.firstChild.getAttribute("num_respuesta") == quizElements[questionCounter]["num_respuesta"]) {
+    if (
+      li.firstChild.textContent == quizElements[questionCounter]["respuesta"]
+    ) {
       li.firstChild.style.backgroundColor = "#3CB371";
     }
   });
 }
 
-// Función para generar las respuestas 
-function generarRespuestas (indice){
-  while (ul1.firstChild){
+// Función para generar las respuestas
+function generarRespuestas(indice) {
+  while (ul1.firstChild) {
     ul1.removeChild(ul1.firstChild);
-  };
+  }
   let counter = 0;
-  quizElements[indice]["answers"].forEach(answer => {
+  quizElements[indice]["answers"].forEach((answer) => {
     let btn = document.createElement("button");
     let li = document.createElement("li");
     btn.className = "answer-btn";
-    btn.setAttribute("num_respuesta", counter);
     btn.textContent = answer;
-    btn.addEventListener("click", () => { 
+    btn.addEventListener("click", () => {
       marcarRespuesta(btn);
+      //Comprueba si se han respondido todas y modifica el boolean
+      if (!todosTienenRespuesta) {
+        todosTienenRespuesta = quizElements.every((elemento) =>
+          elemento.hasOwnProperty("respuesta")
+        );
+      }
+      //Si la variable booleana es true, activamos el botón de check
+      if (todosTienenRespuesta) {
+        btnCheck.disabled = false;
+      }
     });
     counter++;
     li.append(btn);
@@ -60,19 +95,19 @@ const p1 = document.createElement("p");
 p1.textContent = quizElements[questionCounter]["question"];
 
 const ul1 = document.createElement("ul");
-ul1.className = "container-answers"
+ul1.className = "container-answers";
 
 generarRespuestas(questionCounter);
 
 const div2 = document.createElement("div");
 div2.className = "container-footer";
 
-controllersText.forEach(controllerText => {
+controllersText.forEach((controllerText) => {
   let btn = document.createElement("button");
   btn.className = "footer-btn";
   btn.textContent = controllerText;
   div2.append(btn);
-})
+});
 
 body.append(div1);
 div1.append(h2_1, p1, ul1, div2);
@@ -82,35 +117,66 @@ div1.append(h2_1, p1, ul1, div2);
 let footerBtns = document.querySelectorAll(".footer-btn");
 let btnPreview = footerBtns[0];
 let btnNext = footerBtns[1];
+const btnCheck = footerBtns[2];
 // Primer inicio de quiz
-if (p1.textContent == quizElements[0]["question"]){
+if (p1.textContent == quizElements[0]["question"]) {
   btnPreview.disabled = true;
+  btnCheck.disabled = true;
 }
 //Evento de botón preview
-btnPreview.addEventListener("click", () =>{
-  if (questionCounter == quizElements.length -1){
+btnPreview.addEventListener("click", () => {
+  if (questionCounter == quizElements.length - 1) {
     btnNext.disabled = false;
   }
   questionCounter--;
   p1.textContent = quizElements[questionCounter]["question"];
   generarRespuestas(questionCounter);
   marcarCorrecta();
-  if (p1.textContent == quizElements[0]["question"]){
+  if (p1.textContent == quizElements[0]["question"]) {
     btnPreview.disabled = true;
   }
-})
+});
 //Evento de botón Next
-btnNext.addEventListener("click", () =>{
-  if (questionCounter == 0){
+btnNext.addEventListener("click", () => {
+  if (questionCounter == 0) {
     btnPreview.disabled = false;
   }
   questionCounter++;
   p1.textContent = quizElements[questionCounter]["question"];
-  
+
   generarRespuestas(questionCounter);
   marcarCorrecta();
-  if (p1.textContent == quizElements.at(-1)["question"]){
+  if (p1.textContent == quizElements.at(-1)["question"]) {
     btnNext.disabled = true;
   }
+});
+//Evento del boton Check
+btnCheck.addEventListener("click", () => {
+  let respuestasCorrectas = 0;
+  quizElements.forEach((elemento) => {
+    if (elemento["correctAnswer"] == elemento["respuesta"]) {
+      respuestasCorrectas++;
+    }
+  });
+  crearModal(respuestasCorrectas);
+});
+
+function crearModal (respuestasCorrectas){
+  const divModalExt = document.createElement("div");
+  divModalExt.className = "modal";
+  const divModalInt = document.createElement("div");
+  divModalInt.className = "modal-content";
+  const pModalX = document.createElement("p");
+  pModalX.className = "modal-close";
+  const h2Modal = document.createElement("h2");
+  h2Modal.textContent = "RESULTADO";
+  const hrModal = document.createElement("hr");
+  const pModalText = document.createElement("p");
+  pModalText.textContent = respuestasCorrectas + " correct answers from " + quizElements.length;
+
   
-})
+  divModalInt.append(pModalX,h2Modal,hrModal,pModalText);
+  divModalExt.append(divModalInt);
+
+  divModalExt.style.visibility = "visible";
+};
